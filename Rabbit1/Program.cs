d68c3de8196f82c34f28e53fd6e05e24f8c51f7b
user <user@WIN-MKQ26SV0D4C>
2024-10-23 21:36:11 +0300

    Добавьте файлы проекта.

---
+++ Rabbit1/Program.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+namespace Rabbit1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread threadPublisher = new Thread(Publish);
+            Thread threadConsumer = new Thread(Consume);
+            threadPublisher.Start();
+            threadConsumer.Start();
+        }
+        private static void Publish()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "MainQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                if (string.IsNullOrEmpty(message)) continue;
+
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "", routingKey: "MainQueue", basicProperties: null, body: body);
+                Console.WriteLine(" [X] Sent: {0}", message);
+            }
+        }
+        private static void Consume()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "SecondQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Received: {message}");
+            };
+            channel.BasicConsume(queue: "SecondQueue", autoAck: true, consumer: consumer);
+            while (true)
+            {
+                Thread.Sleep(100);
+            }
+        }
+    }
+
+}
---
+++ Rabbit2/Program.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+namespace Rabbit2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread threadPublisher2 = new Thread(Publish);
+            Thread threadConsumer2 = new Thread(Consume);
+            threadPublisher2.Start();
+            threadConsumer2.Start();
+        }
+        private static void Publish()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "SecondQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                if (string.IsNullOrEmpty(message)) continue;
+
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "", routingKey: "SecondQueue", basicProperties: null, body: body);
+                Console.WriteLine(" [X] Sent: {0}", message);
+            }
+        }
+        private static void Consume()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "MainQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Received: {message}");
+            };
+            channel.BasicConsume(queue: "MainQueue", autoAck: true, consumer: consumer);
+            while (true)
+            {
+                Thread.Sleep(100);
+            }
+        }
+    }
+
+}
